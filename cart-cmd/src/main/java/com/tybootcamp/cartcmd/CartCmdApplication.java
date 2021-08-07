4a15e30f92aac70ddde9417f3e8cc203fcd5b8b9
meverg <eminvergili96@gmail.com>
2021-08-07 21:23:46 +0300

    Init cart command service

---
+++ cart-cmd/src/main/java/com/tybootcamp/cartcmd/CartCmdApplication.java
@@ -0,0 +1,12 @@
+package com.tybootcamp.cartcmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartCmdApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CartCmdApplication.class, args);
+  }
+}
---
+++ cart-cmd/src/test/java/com/tybootcamp/cartcmd/CartCmdApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tybootcamp.cartcmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartCmdApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
