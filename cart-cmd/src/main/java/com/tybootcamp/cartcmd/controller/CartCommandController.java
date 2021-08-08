781a3aa513ae4f1bc61d58ab28ed001260fd45dc
meverg <eminvergili96@gmail.com>
2021-08-08 23:56:59 +0300

    Make cart command service compatible with discovery and gateway

---
+++ cart-cmd/src/main/java/com/tybootcamp/cartcmd/controller/CartCommandController.java
@@ -0,0 +1,11 @@
+package com.tybootcamp.cartcmd.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author meverg
+ */
+@RestController
+public class CartCommandController {
+
+}
