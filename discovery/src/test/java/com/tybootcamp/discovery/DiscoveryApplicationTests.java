49f5f75717568b01dedb8c238a6727c0c8901e21
meverg <eminvergili96@gmail.com>
2021-08-08 23:55:23 +0300

    Add discovery service

---
+++ discovery/src/test/java/com/tybootcamp/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tybootcamp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
